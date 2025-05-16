93b095bbdc23036a766fcdb79991a4f6f0cced2f
EC2 Default User <ec2-user@ip-172-31-44-77.ap-south-1.compute.internal>
2025-05-16 07:31:43 +0000

    Initial Terraform project with S3 bucket

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_s3_bucket" "git_demo" {
+  bucket = "git-demo-${random_id.suffix.hex}"
+  acl    = "private"
+
+  tags = {
+    Name = "Git-Demo"
+  }
+}
+
+resource "random_id" "suffix" {
+  byte_length = 4
+}
+
